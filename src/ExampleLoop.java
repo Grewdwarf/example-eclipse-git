8dadaaf0678cf6bdbc5b5f509c48597a9909875e
Love Gruvberg <love.gruvberg@gmail.com>
2021-02-16 14:59:11 +0100

    Included the while loop

--- src/ExampleLoop.java
+++ src/ExampleLoop.java
@@ -4,8 +4,13 @@
 public class ExampleLoop {
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
-			System.out.println(i+1);
-			
+			System.out.println(i+1);	
+		}
+		
+		int counter = 0;
+		while (counter < 10) {
+			System.out.println(counter + 1);
+			counter++;
 		}
 	}
 }
